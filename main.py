e2f8f46054937fee4ac1ba9a20ef08cdfdd7311c
Drazam <146709444+pierre-mazard@users.noreply.github.com>
2023-12-12 13:55:17 +0100

    Ajouts : =====> Modification du bouton mute en ajoutant une animation

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ pygame.init()
 pygame.mixer.init()
 
 #                       Musique de fond
-music = "sounds/epic_dark.mp3"
+music = "sounds\epic_dark.mp3"
 pygame.mixer.music.load(music)
 pygame.mixer.music.play(-1)
 
@@ -24,7 +24,7 @@ height, width  = 800,600
 screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Pendu - Menu du jeu")
 
-#                       Chargement des images de l'animation 
+#                       Chargement des images de l'animation de la potence 
 animation00 = pygame.image.load("images/animation_00.png")
 animation01 = pygame.image.load("images/animation_01.png")
 animation02 = pygame.image.load("images/animation_02.png")
@@ -54,11 +54,20 @@ button_launch_x, button_launch_y = 10, 10
 launch_game_icon = pygame.transform.scale(launch_game_icon, (210, 125))
  
 #                       Icônes du bouton mute
-speaker_on_icon = pygame.image.load("images/speaker_on.png")
-speaker_off_icon = pygame.image.load("images/speaker_off.png")
+speaker_on_icon_00 = pygame.image.load("images/speaker_on_00.png")
+speaker_on_icon_01 = pygame.image.load("images/speaker_on_01.png")
+speaker_on_icon_02 = pygame.image.load("images/speaker_on_02.png")
+speaker_on_icon_03 = pygame.image.load("images/speaker_on_03.png")
+speaker_on_icon_04 = pygame.image.load("images/speaker_on_04.png")
+speaker_on_icon_05 = pygame.image.load("images/speaker_on_05.png")
+speaker_off_icon = pygame.image.load("images\speaker_off.png")
 
+speaker_on_images = [speaker_on_icon_00, speaker_on_icon_01, speaker_on_icon_02, speaker_on_icon_03, speaker_on_icon_04, speaker_on_icon_05]
+current_image_index = 0
 #                       Position du bouton mute
-button_x, button_y = width - speaker_on_icon.get_width() - 10, 10
+image_width = speaker_on_images[0].get_width()
+image_height = speaker_on_images[0].get_height()
+button_x, button_y = width - image_width - 35, 5
 
 #                       Etat de la musique activée 
 music_on = True
@@ -72,20 +81,20 @@ while True:
         elif event.type == pygame.MOUSEBUTTONDOWN:
             # Vérification du clic sur le bouton de mute
             mouse_x, mouse_y = pygame.mouse.get_pos()
-            if button_x <= mouse_x <= button_x + speaker_on_icon.get_width() and \
-                    button_y <= mouse_y <= button_y + speaker_on_icon.get_height():
+            if button_x <= mouse_x <= button_x + image_width and \
+                    button_y <= mouse_y <= button_y + image_height :
                 music_on = not music_on
                 if music_on:
                     pygame.mixer.music.unpause()
                 else:
                     pygame.mixer.music.pause()
                     
-#                       Afficage de l'animation    
+#                       Affichage de l'animation de la potence    
     screen.blit(pictures_list[current_picture], (0,0))
     
 #                       Affichage du bouton mute
     if music_on:
-      screen.blit(speaker_on_icon, (button_x, button_y))
+        screen.blit(speaker_on_images[current_image_index], (button_x, button_y))
     else:
         screen.blit(speaker_off_icon, (button_x, button_y))  
  
@@ -94,6 +103,7 @@ while True:
     
     pygame.display.flip()
 
+    current_image_index = (current_image_index + 1) % len(speaker_on_images)
     current_picture = (current_picture + 1) % len(pictures_list)
     pygame.time.delay(animation_delay)
     clock.tick(60)
